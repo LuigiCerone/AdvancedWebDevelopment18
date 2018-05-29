fbeb51b6e7c173bbe062749f9a537383f1487b6b
luigi.cerone <luigiceroneaq@gmail.com>
2018-05-29 11:07:48 +0200

    Login functionalities added

--- client/js/script.js
+++ client/js/script.js
@@ -16,4 +16,24 @@ $(function () {
             console.log(error);
         }
     });
+
+
+    $('#loginForm').on('submit', function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: "http://localhost:8080/awd18/rest/auth",
+            type: "POST",
+            data: {
+                "email": $('#email').val(),
+                "password": $('#password').val()
+            },
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    })
 });
